9bbd17ac66e60f7889c36cb95f90a86008be2d1e
mardzin <martin.wlod@gmail.com>
2019-11-04 02:09:11 +0100

    Finished ex1 and ex2 from lab3, fixed CMakeLists

---
+++ lab3/ex1/search_proc.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <string.h>
+#include <asm/errno.h>
+#include <errno.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <wait.h>
+#include "utils.c"
+
+// similar to lab2/ex2 but search each new directory in a separate process
+int deep_search_files(char *dirname, int compare_func(char *, char *), char *date)
+{
+    DIR *dir = opendir(dirname);
+    if (dir == NULL)
+    {
+        errno = EIO;
+        perror("Directory doesn't exist");
+        return 1;
+    }
+    struct stat *st = (struct stat *) malloc(sizeof(struct stat));
+    struct dirent *dirent = NULL;
+    char *new_path = NULL;
+    while ((dirent = readdir(dir)))
+    {
+        // create path to dir-entry
+        new_path = (char *) malloc(strlen(dirname) + sizeof("/") + strlen(dirent->d_name));
+        strcpy(new_path, dirname);
+        if (dirname[strlen(dirname)-1] != '/')
+        {
+            strcat(new_path, "/");
+        }
+        strcat(new_path, dirent->d_name);
+        // if new directory found search it for files
+        if (dirent->d_type == DT_DIR && strcmp(dirent->d_name, ".") != 0 && strcmp(dirent->d_name, "..") != 0)
+        {
+            pid_t cpid = fork();
+            // parent process -> wait until child dies, check return status
+            if (cpid)
+            {
+                int cstat;
+                waitpid(cpid, &cstat, 0);
+                if (cstat)
+                {
+                    perror("Something went wrong during searching directories");
+                    return 2;
+                }
+
+            }
+            else
+            {
+                printf("Child process. Dir: %s, PPID: %d, PID: %d\n", dirent->d_name, getppid(), getpid());
+                deep_search_files(new_path, compare_func, date);
+                free(st);
+                free(new_path);
+                closedir(dir);
+                return 0;
+            }
+        }
+        else if (dirent->d_type == DT_REG)
+        {
+            stat(new_path, st);
+            // check if the last modified date of the file is before/equal/after date provided
+            if (compare_func(format_timespec(st->st_mtim), date) == 0)
+            {
+                char *abs_path = new_path;
+                if (new_path[0] != '/')
+                {
+                    abs_path = to_absolute(new_path);
+                }
+                print_file_details(abs_path, format_timespec(st->st_mtim), st);
+            }
+        }
+        free(new_path);
+    }
+    free(st);
+    closedir(dir);
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        errno = EINVAL;
+        perror("Usage: ./search_stat path/to/dir ('>' or '<' or '=') 'yyyy-MM-dd hh:mm:ss'");
+        return 1;
+    }
+
+    char *dirname = argv[1];
+    char *sign = argv[2];
+    if (strlen(sign) != 1)
+    {
+        errno = EINVAL;
+        perror("Usage: ./search_stat path/to/dir ('>' or '<' or '=') 'yyyy-MM-dd hh:mm:ss'");
+        return 2;
+    }
+    char *date = argv[3];
+    if (validate_date(date) != 0)
+    {
+        return 3;
+    }
+    switch (sign[0])
+    {
+        case '<':
+            return deep_search_files(dirname, less_than, date);
+        case '>':
+            return deep_search_files(dirname, greater_than, date);
+        case '=':
+            return deep_search_files(dirname, equal, date);
+        default:
+            errno = EINVAL;
+            perror("Only '<', '>' and '=' are supported as a second argument.");
+            return 4;
+    }
+}
---
+++ lab3/ex2/cmd_interpreter.c
@@ -0,0 +1,154 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <zconf.h>
+#include <wait.h>
+#include <ctype.h>
+
+static const int MAX_ARGS = 5;
+static const int BUFFER_SIZE = 1024;
+
+// modifies source string
+void trim_whitespace(char **str)
+{
+    // leading
+    while (isspace((unsigned char) **str))
+    {
+        (*str)++;
+    }
+    if (**str == 0)
+    {
+        return;
+    }
+
+    // trailing
+    char *end = *str + strlen(*str) - 1;
+    while (isspace((unsigned char) *end))
+    {
+        end--;
+    }
+    end[1] = '\0';
+}
+
+char **tokenize(char *str)
+{
+    // program name + args + NULL
+    char **tokens = (char **) calloc(MAX_ARGS + 2, sizeof(char *));
+    unsigned long tstart = 0;
+    int tcounter = 0;
+    short is_quoted = 0;
+    for (unsigned long i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == '\"')
+        {
+            // ending quote
+            if (is_quoted)
+            {
+                tokens[tcounter] = (char *) calloc(BUFFER_SIZE, sizeof(char));
+                // trim quotes
+                strncpy(tokens[tcounter++], str + tstart, i - tstart);
+                // next token starts after quote and space
+                tstart += i - tstart + 2;
+                i++;
+                is_quoted = 0;
+            }
+            // starting quote
+            else
+            {
+                is_quoted = 1;
+                tstart++;
+            }
+        }
+        else if (str[i] == ' ' && !is_quoted)
+        {
+            tokens[tcounter] = (char *) calloc(BUFFER_SIZE, sizeof(char));
+            // copy whole word up to the point of space char
+            strncpy(tokens[tcounter++], str + tstart, i - tstart);
+            // starting index of new word is after space char
+            tstart += i - tstart + 1;
+
+        }
+        // last token
+        else if (i == strlen(str) - 1)
+        {
+            tokens[tcounter] = (char *) calloc(BUFFER_SIZE, sizeof(char));
+            strncpy(tokens[tcounter++], str + tstart, i - tstart + 1);
+        }
+    }
+    return tokens;
+}
+
+void free_dp(char **dp) {
+    int i = 0;
+    while (dp[i])
+    {
+        free(dp[i++]);
+    }
+    free(dp);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        errno = EINVAL;
+        perror("Usage: ./cmd_interpreter path/to/file.txt");
+        return 1;
+    }
+
+    char *filename = argv[1];
+    FILE *file = fopen(filename, "r+");
+    if (!file)
+    {
+        errno = EIO;
+        perror("Opening of file failed");
+        return 2;
+    }
+
+    char *line = NULL;
+    size_t len = 0;
+    long linenum = 1;
+
+    while (getline(&line, &len, file) != -1)
+    {
+        trim_whitespace(&line);
+        char **pargs = tokenize(line);
+
+        pid_t cpid = fork();
+        // parent process -> wait until child dies, check return status
+        if (cpid)
+        {
+            int cstat;
+            waitpid(cpid, &cstat, 0);
+            if (cstat)
+            {
+                fprintf(stderr, "Execution of program %s from line %ld failed", pargs[0], linenum);
+                free(line);
+                fclose(file);
+                free_dp(pargs);
+                return 3;
+            }
+
+        }
+        // child process -> execute saved command with args
+        else
+        {
+            printf("%ld. Executing %s command, PPID: %d, PID: %d\n", linenum, pargs[0], getppid(), getpid());
+            int status = execvp(pargs[0], pargs);
+            if (status < 0)
+            {
+                perror(pargs[0]);
+                free(line);
+                fclose(file);
+                free_dp(pargs);
+                return 4;
+            }
+        }
+        linenum++;
+        free_dp(pargs);
+    }
+    free(line);
+    fclose(file);
+    return 0;
+}
