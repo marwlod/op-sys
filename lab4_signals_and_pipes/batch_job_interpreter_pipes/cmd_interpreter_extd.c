f89d80f5734e3ced80e0c06d9bab3c55fd9cc08e
mardzin <martin.wlod@gmail.com>
2019-11-29 14:37:38 +0100

    [lab4] batch job interpreter w/pipes - fixed memory leaks, simplified code

--- lab4_signals_and_pipes/batch_job_interpreter_pipes/cmd_interpreter_extd.c
+++ lab4_signals_and_pipes/batch_job_interpreter_pipes/cmd_interpreter_extd.c
@@ -38,7 +38,7 @@ void trim_whitespace(char **str)
 
 char **tokenize(char *str)
 {
-    // program name + args + NULL
+    // max programs * (program name + args + NULL)
     char **tokens = (char **) calloc(MAX_PROGS_PIPED * (MAX_ARGS + 2), sizeof(char *));
     unsigned long tstart = 0;
     int tcounter = 0;
@@ -93,16 +93,10 @@ void free_dp(char **dp) {
     free(dp);
 }
 
-void free_tp(char ***tp) {
-    int i = 0, j = 0;
-    while (tp[i])
-    {
-        while (tp[i][j]) {
-            free(tp[i][j++]);
-        }
-        free(tp[i++]);
+void free_pargs(char ***pargs) {
+    for (int i = 0; i < MAX_PROGS_PIPED; i++) {
+        free_dp(pargs[i]);
     }
-    free(tp);
 }
 
 int main(int argc, char *argv[])
@@ -131,13 +125,10 @@ int main(int argc, char *argv[])
     {
         trim_whitespace(&line);
         char **tokens = tokenize(line);
-        // additional space for NULL
-        char ***pargs = (char ***) calloc(MAX_PROGS_PIPED + 1, sizeof(char **));
-        for (int i = 0; i < MAX_PROGS_PIPED + 1; i++) {
+        char **pargs[MAX_PROGS_PIPED];
+        for (int i = 0; i < MAX_PROGS_PIPED; i++)
+        {
             pargs[i] = (char **) calloc(MAX_ARGS, sizeof(char *));
-            for (int j = 0; j < MAX_ARGS; j++) {
-                pargs[i][j] = (char *) calloc(BUFFER_SIZE, sizeof(char));
-            }
         }
 
         int progNum = 0;
@@ -147,63 +138,73 @@ int main(int argc, char *argv[])
         {
             if (strcmp(*ttrav, "|") != 0)
             {
+                pargs[progNum][argNum] = (char *) calloc(BUFFER_SIZE, sizeof(char));
                 memcpy(pargs[progNum][argNum++], *ttrav, strlen(*ttrav));
             }
             else
             {
-                pargs[progNum][argNum] = NULL;
                 argNum = 0;
                 progNum++;
             }
             ttrav++;
-            // last command before end of line
-            if (!(*ttrav))
-            {
-                pargs[progNum][argNum] = NULL;
-                pargs[progNum+1] = NULL;
-            }
         }
         free_dp(tokens);
         int totalProgs = progNum + 1;
 
         int fds[2 * totalProgs];
-        for (int i = 0; i < totalProgs; i++) {
-            if (pipe(fds + 2 * i)) {
+        for (int i = 0; i < totalProgs; i++)
+        {
+            if (pipe(fds + 2 * i))
+            {
                 perror("Pipe creation failed");
-                return 5;
+                free(line);
+                fclose(file);
+                free_pargs(pargs);
+                return 3;
             }
         }
         int currp = 0;
-        while (pargs[currp]) {
+        while (*pargs[currp])
+        {
             pid_t cpid = fork();
-            if (cpid < 0) {
-                fprintf(stderr, "Execution of program %s from line %ld failed", pargs[currp][0], linenum);
+            if (cpid < 0)
+            {
+                fprintf(stderr, "Execution of program %s from line %ld failed\n", pargs[currp][0], linenum);
                 free(line);
                 fclose(file);
-                free_tp(pargs);
-                return 3;
+                free_pargs(pargs);
+                return 4;
             }
             // child process
             else if (cpid == 0)
             {
                 // if not last program
-                if (pargs[currp+1]) {
-                    if (dup2(fds[currp * 2 + 1], STDOUT_FILENO) < 0) {
-                        fprintf(stderr, "Dup2 failed");
-                        return 6;
+                if (*pargs[currp+1])
+                {
+                    if (dup2(fds[currp * 2 + 1], STDOUT_FILENO) < 0)
+                    {
+                        fprintf(stderr, "dup2 failed when trying to redirect output of current program to STDOUT\n");
+                        free(line);
+                        fclose(file);
+                        free_pargs(pargs);
+                        return 5;
                     }
                 }
                 // if not first program
-                if (currp > 0) {
-                    if (dup2(fds[(currp-1) * 2], STDIN_FILENO) < 0) {
-                        fprintf(stderr, "Dup2 failed");
+                if (currp > 0)
+                {
+                    if (dup2(fds[(currp-1) * 2], STDIN_FILENO) < 0)
+                    {
+                        fprintf(stderr, "dup2 failed when trying to redirect input of previous program to STDIN\n");
+                        free(line);
+                        fclose(file);
+                        free_pargs(pargs);
                         return 6;
                     }
                 }
-                // close all fds
-                for (int i = 0; i < 2 * totalProgs; i++) {
-                    close(fds[i]);
-                }
+                // close all fds for pipes
+                for (int i = 0; i < 2 * totalProgs; i++) close(fds[i]);
+
                 printf("%ld.%d. Executing '%s', PPID: %d, PID: %d\n", linenum, currp, line, getppid(), getpid());
                 int status = execvp(pargs[currp][0], pargs[currp]);
                 if (status < 0)
@@ -211,20 +212,17 @@ int main(int argc, char *argv[])
                     perror(pargs[currp][0]);
                     free(line);
                     fclose(file);
-                    free_tp(pargs);
-                    return 4;
+                    free_pargs(pargs);
+                    return 7;
                 }
             }
             currp++;
         }
 
-        for (int i = 0; i < 2 * totalProgs; i++) {
-            close(fds[i]);
-        }
-        for (int i = 0; i < totalProgs; i++) {
-            wait(NULL);
-        }
+        for (int i = 0; i < 2 * totalProgs; i++) close(fds[i]);
+        for (int i = 0; i < totalProgs; i++) wait(NULL);
         linenum++;
+        free_pargs(pargs);
     }
     free(line);
     fclose(file);
